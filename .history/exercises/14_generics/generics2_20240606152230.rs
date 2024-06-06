7d3622a3756cf02653e2a754186011618fddf7ef
xiaoyi <xiaoyi@xiaoyi.localdomain>
2024-06-06 15:45:24 +0800

    Finished 12-15 chapter.

---
+++ .history/exercises/14_generics/generics2_20240606152230.rs
@@ -0,0 +1,44 @@
+// generics2.rs
+//
+// This powerful wrapper provides the ability to store a positive integer value.
+// Rewrite it using generics so that it supports wrapping ANY type.
+//
+// Execute `rustlings hint generics2` or use the `hint` watch subcommand for a
+// hint.
+
+struct Wrapper<T> {
+    value: T,
+}
+
+impl<T> Wrapper<T> {
+    pub fn new(value: T) -> Self {
+        Wrapper { value }
+    }
+}
+
+impl Wrapper<u32> {
+    pub fn add_42(self) -> u32 {
+        self.value + 42
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn u32_add_42() {
+        let w = Wrapper::new(10);
+        assert_eq!(w.add_42(), 52);
+    }
+
+    #[test]
+    fn store_u32_in_wrapper() {
+        assert_eq!(Wrapper::new(42).value, 42);
+    }
+
+    #[test]
+    fn store_str_in_wrapper() {
+        assert_eq!(Wrapper::new("Foo").value, "Foo");
+    }
+}
